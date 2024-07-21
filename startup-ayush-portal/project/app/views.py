6adbca822ba4056baee48e4a58ea088c4f41966b
sanjanaharishpoojary <sanjanahpoojary@gmail.com>
2024-07-21 19:56:26 +0530

    views

--- startup-ayush-portal/project/app/views.py
+++ startup-ayush-portal/project/app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import json
+from .models import Animation
 
+def load_animation_data():
+    with open('path/to/typing.json', 'r') as file:
+        data = json.load(file)
+    
+    animation = Animation(name=data['nm'], data=data)
+    animation.save()
+    
 def index(request):
     return render(request, 'app/index.html')

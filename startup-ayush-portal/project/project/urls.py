33fe46794bceaa44864b695713592a9b1e804183
sanjithsmurthy <sanjithsmurthy@gmail.com>
2024-07-23 11:51:01 +0530

    Signin Signup Pages added by Satvik and a few lines in homepage

--- startup-ayush-portal/project/project/urls.py
+++ startup-ayush-portal/project/project/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from app.views import index
+from django.urls import path,include
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('app.urls')),
 ]

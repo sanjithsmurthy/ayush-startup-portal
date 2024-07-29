cc02855c642c9a90ff6318311e5fa9a3f57bc0d1
sanjithsmurthy <sanjithsmurthy@gmail.com>
2024-07-29 10:24:55 +0530

    Finished Contact Page, about page and all other pages.

--- startup-ayush-portal/project/project/settings.py
+++ startup-ayush-portal/project/project/settings.py
@@ -43,11 +43,11 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
 
-    
+
 ]
 
-SITE_ID=1
-SOCIALACCOUNT_LOGIN_ON_GET=True
+SITE_ID = 1
+SOCIALACCOUNT_LOGIN_ON_GET = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'allauth.account.middleware.AccountMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -112,13 +112,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 AUTHENTICATION_BACKENDS = [
-    
+
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
-    
+
 ]
 
 # Internationalization
@@ -142,3 +142,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '<startupayushportal18@gmail.com>'
+EMAIL_HOST_PASSWORD = 'Yeeeessss!2>'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
